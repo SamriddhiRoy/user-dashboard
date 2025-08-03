5404cb06ca2e1c2bd963461a3f30e92ad01d84dd
SamriddhiRoy <samriddhi7564@gmail.com>
2025-08-03 23:27:36 +0530

    u2

--- dash/next.config.ts
+++ dash/next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
-  experimental: {
-    serverComponentsExternalPackages: ['@neondatabase/serverless'],
-  },
+  
   webpack: (config) => {
     config.externals.push({
       '@neondatabase/serverless': '@neondatabase/serverless',
